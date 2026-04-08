167fcd46f676decc3140fb2a278d47bf00385efc
varshasri k <168010651+varshasri2005@users.noreply.github.com>
2026-04-08 20:32:10 +0530

    Add files via upload

---
+++ Ex 1/EX1.py
@@ -0,0 +1,16 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv("./.env")
+
+client = OpenAI(
+    api_key=os.environ.get("GROQ_API_KEY"),
+    base_url="https://api.groq.com/openai/v1",
+)
+
+response = client.responses.create(
+    input="Explain the importance of fast language models",
+    model="openai/gpt-oss-20b",
+)
+print(response.output_text)
